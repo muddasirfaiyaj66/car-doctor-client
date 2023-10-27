7e771a7633158c92e6c17e1d81e6d336676825c7
Muddasir-Faiyaj <muddasirfaiyaj66@gmail.com>
2023-10-27 09:27:08 +0600

    navbar and footer complete

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import Navbar from "../Pages/Shared/Navbar/Navbar";
 
 
 const Main = () => {
     return (
         <div>
+           <div className="max-w-screen-xl mx-auto "> 
+           <Navbar></Navbar>
+           </div>
            <Outlet></Outlet> 
            <Footer></Footer>
         </div>
--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -3,30 +3,29 @@
 const Footer = () => {
     return (
         <div>
-            <footer className="footer p-10 bg-base-200 text-base-content">
+            <footer className="footer p-10 bg-black text-white text-base-content">
   <aside>
-    <img src="/src/assets/logo.svg" alt="" />
-    <p>Car Doctor<br/>Providing reliable tech since 1992</p>
+    <img src="/src/assets/footerLogo.png" alt="" />
+    <p>Edwin Diaz is a software and web <br /> technologies engineer, a life coach <br /> trainer who is also a serial .<br/>Providing reliable tech since 1992</p>
   </aside> 
   <nav>
-    <header className="footer-title">Services</header> 
-    <a className="link link-hover">Branding</a> 
-    <a className="link link-hover">Design</a> 
-    <a className="link link-hover">Marketing</a> 
-    <a className="link link-hover">Advertisement</a>
-  </nav> 
-  <nav>
-    <header className="footer-title">Company</header> 
-    <a className="link link-hover">About us</a> 
+    <header className="footer-title text-white font-bold">About</header> 
+    <a className="link link-hover">Home </a> 
+    <a className="link link-hover">Service</a> 
     <a className="link link-hover">Contact</a> 
-    <a className="link link-hover">Jobs</a> 
-    <a className="link link-hover">Press kit</a>
+   
   </nav> 
   <nav>
-    <header className="footer-title">Legal</header> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
+    <header className="footer-title text-white font-bold">Company</header> 
+    <a className="link link-hover">Why car doctor</a> 
+    <a className="link link-hover">About</a> 
+    
+  </nav> 
+  <nav>
+    <header className="footer-title  text-white">Support</header> 
+    <a className="link link-hover">Support Center</a> 
+    <a className="link link-hover">Feedback</a> 
+    <a className="link link-hover">Accessibility</a>
   </nav>
 </footer>
         </div>
